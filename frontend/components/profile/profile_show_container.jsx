8c398c73c69b9b708962091f062720bc99a91b2e
Vladimir-19 <v.s.solovey@gmail.com>
2020-12-16 23:57:52 -0500

    board

--- frontend/components/profile/profile_show_container.jsx
+++ frontend/components/profile/profile_show_container.jsx
@@ -1,21 +1,46 @@
 // import React from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 
-import { openModal, closeModal } from "../../actions/modal_actions";
+// import { openModal, closeModal } from "../../actions/modal_actions";
+// import { fetchAllUsers, fetchSingleUser } from "../../actions/user_actions";
+// import ProfileShow from "./profile_show";
+
+// const mapStateToProps = (state, ownProps) => ({
+//     currentUser: state.entities.users[state.session.id],
+//     users: Object.values(state.entities.users),
+//     email: ownProps.match.params.email,
+//     boards: Object.values(state.entities.boards),
+//     boardsPins: state.entities.boardsPins,
+//     pins: Object.values(state.entities.pins),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//     fetchBoards: () => dispatch(fetchBoards()),
+//     fetchAllUsers: () => dispatch(fetchAllUsers()),
+//     fetchSingleUser: id => dispatch(fetchSingleUser(id)),
+//     openModal: modal => dispatch(openModal(modal)),
+//     closeModal: () => dispatch(closeModal())
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(ProfileShow);
+
+import { connect } from "react-redux";
+import React from "react";
+
 import { fetchAllUsers, fetchSingleUser } from "../../actions/user_actions";
+import { openModal, closeModal } from "../../actions/modal_actions";
 import ProfileShow from "./profile_show";
 
 const mapStateToProps = (state, ownProps) => ({
     currentUser: state.entities.users[state.session.id],
     users: Object.values(state.entities.users),
-    email: ownProps.match.params.email,
+    username: ownProps.match.params.username,
     boards: Object.values(state.entities.boards),
     boardsPins: state.entities.boardsPins,
     pins: Object.values(state.entities.pins),
 });
 
 const mapDispatchToProps = dispatch => ({
-    fetchBoards: () => dispatch(fetchBoards()),
     fetchAllUsers: () => dispatch(fetchAllUsers()),
     fetchSingleUser: id => dispatch(fetchSingleUser(id)),
     openModal: modal => dispatch(openModal(modal)),
