71bfd9cc1b5b2149ad0fb6f9fe0c1c76c87dcde3
Vladimir-19 <v.s.solovey@gmail.com>
2020-12-27 14:22:53 -0500

    grid

--- frontend/components/board/board_index.jsx
+++ frontend/components/board/board_index.jsx
@@ -1,67 +1,3 @@
-// import React, { Component } from 'react';
-// import BoardIndexItem from './board_index_item';
-
-
-// class BoardIndex extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.openEditBoard = this.openEditBoard.bind(this);
-//   }
-
-//   openEditBoard(e, boardId) {
-//     e.preventDefault();
-//     this.props.openEditBoard(boardId);
-//   }
-
-//   render() {
-//     const { boards, pins, currentUser, user, newBoard } = this.props;
-//     const boardIndexItems = (boards.length > 0) ? (
-//       boards.map(board => {
-//         const prevPinIds = (board.pinIds) ? (
-//           board.pinIds.slice(0, 6)
-//         ) : (
-//           board.pinIds
-//         );
-//         const previewPins = prevPinIds.map((pinId) => {
-//           return pins[pinId];
-//         });
-//         return (
-//           <BoardIndexItem
-//             key={board.id}
-//             board={board}
-//             pins={previewPins}
-//             currentUser={currentUser}
-//             user={user}
-//             openEditBoard={this.openEditBoard}
-//           />
-//         )
-//       })
-//     ) : (
-//       null // swap with add button using newBoard
-//     );
-
-//     return (
-//       <div className="board-index">
-//         {boardIndexItems}
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//         <div className="board-index-item filler"></div>
-//       </div>
-//     )
-//   }
-// }
-
-
-// export default BoardIndex;
 import React, { Component } from 'react';
 import BoardIndexItem from './board_index_item';
 
@@ -84,8 +20,8 @@ class BoardIndex extends Component {
         const prevPinIds = (board.pinIds) ? (
           board.pinIds.slice(0, 6)
         ) : (
-            board.pinIds
-          );
+          board.pinIds
+        );
         const previewPins = prevPinIds.map((pinId) => {
           return pins[pinId];
         });
@@ -101,8 +37,8 @@ class BoardIndex extends Component {
         )
       })
     ) : (
-        null // swap with add button using newBoard
-      );
+      null // swap with add button using newBoard
+    );
 
     return (
       <div className="board-index">
@@ -126,3 +62,68 @@ class BoardIndex extends Component {
 
 
 export default BoardIndex;
+
+// import React, { Component } from 'react';
+// import BoardIndexItem from './board_index_item';
+
+
+// class BoardIndex extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.openEditBoard = this.openEditBoard.bind(this);
+//   }
+
+//   openEditBoard(e, boardId) {
+//     e.preventDefault();
+//     this.props.openEditBoard(boardId);
+//   }
+
+//   render() {
+//     const { boards, pins, currentUser, user, newBoard } = this.props;
+//     const boardIndexItems = (boards.length > 0) ? (
+//       boards.map(board => {
+//         const prevPinIds = (board.pinIds) ? (
+//           board.pinIds.slice(0, 6)
+//         ) : (
+//             board.pinIds
+//           );
+//         const previewPins = prevPinIds.map((pinId) => {
+//           return pins[pinId];
+//         });
+//         return (
+//           <BoardIndexItem
+//             key={board.id}
+//             board={board}
+//             pins={previewPins}
+//             currentUser={currentUser}
+//             user={user}
+//             openEditBoard={this.openEditBoard}
+//           />
+//         )
+//       })
+//     ) : (
+//         null // swap with add button using newBoard
+//       );
+
+//     return (
+//       <div className="board-index">
+//         {boardIndexItems}
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//       </div>
+//     )
+//   }
+// }
+
+
+// export default BoardIndex;
